6376d49513d6ca36b339e04a582223d7f6c74486
pranavlawrence <pranavlawrence40@gmail.com>
2019-02-01 13:51:02 +0530

    coding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  todos = [
+  {
+    label: 'do shoes',
+    done: false,
+    priority: 3
+  },
+  {
+    label: 'do dishes',
+    done: true,
+    priority: 1
+  },
+  {
+    label: 'do clothes',
+    done: false,
+    priority: 2
+  },
+  {
+    label: 'cook lunch',
+    done: true,
+    priority: 5
+  },
+  {
+    label: 'reading',
+    done: false,
+    priority: 5
+  },
+];
 }
